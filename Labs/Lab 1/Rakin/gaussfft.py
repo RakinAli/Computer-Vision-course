5c7e62e2bb9d19189854a579880deee3ba1749d0
Rakin <54189419+RakinAli@users.noreply.github.com>
2023-11-13 18:43:19 +0100

    Question 14 took forever fucking hell

--- Labs/Lab 1/Rakin/gaussfft.py
+++ Labs/Lab 1/Rakin/gaussfft.py
@@ -1,7 +1,27 @@
 import numpy as np
 from numpy.fft import fft2, ifft2, fftshift
 
-def gaussfft(pic, t):
-    # Fill in your code here
-    # result = ...  
+
+def gaussfft(pic, t=1.0):
+    # Generate a filter based on a sampled version of the Gaussian function
+    width, height = pic.shape
+    x_values = np.arange(-width / 2, width / 2)
+    y_values = np.arange(-height / 2, height / 2)
+    x, y = np.meshgrid(x_values, y_values)
+
+    g_function = (1/2*np.pi*t)*np.exp(-(x**2+y**2)/(2*t)) 
+    g_function = g_function/np.sum(g_function) # normalize!
+
+
+    # Fourier transform the original image and the filter
+    F_hat = fft2(pic)
+    G_hat = fft2(g_function)
+
+    # Multiply the Fourier transforms
+    res = F_hat*G_hat
+
+    # Inverse Fourier transform
+    result = ifft2(res)
+    
+
     return result
