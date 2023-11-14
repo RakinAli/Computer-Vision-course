3a87545d718102bd78500c55b750fe33e5172615
Rakin <54189419+RakinAli@users.noreply.github.com>
2023-11-14 19:25:39 +0100

    Question 19-20

--- Labs/Lab 1/Rakin/gaussfft.py
+++ Labs/Lab 1/Rakin/gaussfft.py
@@ -1,27 +1,20 @@
 import numpy as np
-from numpy.fft import fft2, ifft2, fftshift
+from numpy.fft import fft2, ifft2
 
 
 def gaussfft(pic, t=1.0):
-    # Generate a filter based on a sampled version of the Gaussian function
-    width, height = pic.shape
-    x_values = np.arange(-width / 2, width / 2)
-    y_values = np.arange(-height / 2, height / 2)
-    x, y = np.meshgrid(x_values, y_values)
-
-    g_function = (1/2*np.pi*t)*np.exp(-(x**2+y**2)/(2*t)) 
-    g_function = g_function/np.sum(g_function) # normalize!
-
-
-    # Fourier transform the original image and the filter
     F_hat = fft2(pic)
-    G_hat = fft2(g_function)
+    width, height = pic.shape
+    x_range = range(int(-width / 2), int(-width / 2) + width)
+    y_range = range(int(-height / 2), int(-height / 2) + height)
+    x, y = np.meshgrid(x_range, y_range)
 
-    # Multiply the Fourier transforms
-    res = F_hat*G_hat
+    gaussian_filter = (1 / (2 * np.pi * t)) * np.exp(-(x**2 + y**2) / (2 * t))
+    gaussian_filter = gaussian_filter / np.sum(gaussian_filter)
 
-    # Inverse Fourier transform
+    filter_hat = np.abs(fft2(gaussian_filter))
+
+    res = F_hat * filter_hat
     result = ifft2(res)
-    
 
     return result
